1ebda323451fbdf3932f8eb0132c4604cd2099ec
olade <olusegunodewole1@gmail.com>
2025-08-02 16:43:57 +0300

    local-recipe-edit6

--- p4a_recipes/llama_cpp_python/__init__.py
+++ p4a_recipes/llama_cpp_python/__init__.py
@@ -3,25 +3,31 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 class LlamaCppPythonRecipe(CompiledComponentsPythonRecipe):
     """
     Recipe for compiling llama-cpp-python.
-    This version applies a patch to remove the incompatible '-march=native' flag.
+    This version uses an explicit self.apply_patch() call to ensure
+    the '-march=native' flag is removed.
     """
-    
+
     version = '0.2.20'
     url = f'https://github.com/abetlen/llama-cpp-python/archive/refs/tags/v{version}.tar.gz'
     name = 'llama-cpp-python'
-    
+
     depends = ['numpy', 'typing_extensions', 'diskcache', 'scikit-build-core']
-    
-    # --- THIS IS THE FIX ---
-    # Apply the patch file we just created.
-    patches = ['disable_native.patch']
-    
+
     site_packages_name = 'llama_cpp'
 
+    def prebuild_arch(self, arch):
+        """
+        This method is called before the build. We explicitly apply our patch here.
+        """
+        super().prebuild_arch(arch)
+        self.apply_patch('disable_native.patch', arch.arch)
+
     def get_recipe_env(self, arch):
+        """
+        Set the environment variables for the build.
+        """
         env = super().get_recipe_env(arch)
-        
-        # We no longer need LLAMA_NATIVE=OFF, but the other flags are still good.
+
         cmake_args = [
             '-DCMAKE_BUILD_TYPE=Release',
             '-DLLAMA_CUBLAS=OFF',
@@ -31,9 +37,9 @@ class LlamaCppPythonRecipe(CompiledComponentsPythonRecipe):
             '-DLLAMA_BUILD_TESTS=OFF',
             '-DLLAMA_BUILD_EXAMPLES=OFF',
         ]
-        
+
         env['SKBUILD_CMAKE_ARGS'] = ' '.join(cmake_args)
-        
+
         return env
 
 # This line is essential
